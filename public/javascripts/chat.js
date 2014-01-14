108e668cdb22d135ec9044ab54df893433c347e3
Matthew Woodard <MadMat41888@aol.com>
2014-01-13 17:34:27 -0800

    Added room changing

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -5,3 +5,21 @@ var Chat = function(socket){
 Chat.prototype.sendMessage = function(message){
   this.socket.emit('message', message);
 };
+
+Chat.prototype.changeNickname = function(nickname){
+  this.socket.emit('nicknameChangeRequest', nickname);
+};
+
+Chat.prototype.processCommand = function(command, callback){
+  if(command.slice(0,6) === '/nick '){
+    this.changeNickname(input.slice(6));
+  } else if (command.slice(0,6) === '/join '){
+    this.joinRoom(input.slice(6));
+  } else {
+    callback("Unrecognized Command");
+  }
+};
+
+Chat.prototype.joinRoom = function(room) {
+  this.socket.emit('changeRoom', room);
+};
--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -6,18 +6,47 @@ var getMessage = function(){
   return $('#new-msg').val();
 };
 
-var sendMessage = function(event){
+var handleInput = function(event){
   event.preventDefault();
-  chat.sendMessage(getMessage());
+  input = getMessage();
+  if(input.slice(0,1) === '/'){
+    chat.processCommand(input, function(error) {
+      alert(error);
+    });
+  } else {
+    sendMessage(input);
+  }
   $('#new-msg').val('');
+};
+
+var sendMessage = function(event){
+  chat.sendMessage(getMessage());
 };
 
 var addMessage = function(message){
   $message = $('<p>').text(message)
-  $('#msg-box').prepend($message);
+  $('#msg-box').append($message);
+  $('#msg-box').scrollTop($('#msg-box')[0].scrollHeight);
+};
+
+var changeNickname = function(nickname){
+  chat.changeNickname(nickname);
+  $('#new-msg').val('');
+};
+
+var renderList = function(list){
+  $list = $('#user-list');
+  $list.html('');
+  list.forEach(function(name){
+      $list.append($('<div>').text(name));
+  });
 };
 
 $(function(){
   socket.on('message', addMessage);
-  $('#msg-form').on('submit', sendMessage);
+  socket.on('nicknameList', renderList);
+  socket.on('newRoom', function(room) {
+    $('#current-room').text("Current Room: " + room);
+  });
+  $('#msg-form').on('submit', handleInput);
 });
